757b683478447ba270e35d9d537eb9e466053b34
youssef ismail <youssefismail09@gmail.com>
2026-07-12 15:23:04 +0100

    feat(drivers): add  implementations for DcMotorDriver and ServoDriver

---
+++ include/ServoDriver.h
@@ -0,0 +1,31 @@
+#ifndef TRINOVA_SERVO_DRIVER_H
+#define TRINOVA_SERVO_DRIVER_H
+
+#include "IRobotDriver.h"
+
+
+class ServoDriver : public IRobotDriver {
+public:
+    ServoDriver();
+
+    bool begin(const RobotConfig& config) override;
+
+    void forward(uint8_t speed) override;
+    void backward(uint8_t speed) override;
+    void turnLeft(uint8_t speed) override;
+    void turnRight(uint8_t speed) override;
+    void stop() override;
+
+    bool moveServo(uint8_t channel, uint8_t angle, uint16_t durationMs) override;
+
+    void emergencyStop() override;
+    void clearEmergencyStop() override;
+
+    void update() override;
+
+private:
+    RobotConfig _config;
+    bool _emergencyActive = false;
+};
+
+#endif
